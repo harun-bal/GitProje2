815a7dae09ae5e1900221e89de329b5859b031a6
HARUN <harunbal1984@gmail.com>
2022-07-04 21:54:33 +0300

    ikinci işlem yapıldı

--- src/Ogrenci.java
+++ src/Ogrenci.java
@@ -2,6 +2,6 @@ public class Ogrenci {
     public static void main(String[] args) {
         System.out.println("merhaba dünya2");
         System.out.println("merhaba dünya3");
-
+        System.out.println("hb");
     }
 }
